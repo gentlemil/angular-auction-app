78f3209057365ba69cbb82d64e89ac7db5df36c7
milosz bukala <milosz.bukala@princity.com>
2020-09-01 08:01:50 +0200

    Create service ProductService, navigation, components ProductItemComponent, HomeComponent and ProductDetailComponent. Need to solve a problem of material-icon-css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { ProductService } from './shared/product.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
